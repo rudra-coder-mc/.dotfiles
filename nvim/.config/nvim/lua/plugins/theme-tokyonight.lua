84a3021178a60a46d311545abfea82cade07decf
atul modi <forcoding255@gmail.com>
2024-09-09 19:00:26 +0530

    improve

--- nvim/.config/nvim/lua/plugins/theme-tokyonight.lua
+++ nvim/.config/nvim/lua/plugins/theme-tokyonight.lua
@@ -2,6 +2,11 @@ return {
 
   'folke/tokyonight.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
+  transparent = true,
+  styles = {
+    sidebars = 'transparent',
+    floats = 'transparent',
+  },
   init = function()
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
